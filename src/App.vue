851474f74dde0155a1dfaec7ef3c56d6700a477c
kamoto <kamototakahiro@kamototakahironoMacBook-Air.local>
2022-01-30 23:22:51 +0900

    タイトル等の修正

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,13 @@
       color="primary"
       dark
     >
+    <span>MUSIC SEARCH</span>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
         />
       </div>
 
@@ -32,8 +22,7 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Home</span>
       </v-btn>
     </v-app-bar>
 
